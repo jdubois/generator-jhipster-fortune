86035d338c93af8d5d58d92d1667495068ddff2e
Julien Dubois <julien.dubois@gmail.com>
2017-03-29 12:19:37 +0200

    Fix eslint issues

--- generators/app/index.js
+++ generators/app/index.js
@@ -27,16 +27,16 @@ module.exports = generator.extend({
         const done = this.async();
 
         if (jhipsterVar.clientFramework !== 'angular1') {
-          this.log(chalk.red('Error!') + ' The JHipster Fortune module only works with AngularJS 1');
-          process.exit(1);
+            this.log(chalk.red('Error! The JHipster Fortune module only works with AngularJS 1'));
+            process.exit(1);
         }
         if (jhipsterVar.databaseType !== 'sql') {
-          this.log(chalk.red('Error!') + ' The JHipster Fortune module only works with SQL databases');
-          process.exit(1);
+            this.log(chalk.red('Error! The JHipster Fortune module only works with SQL databases'));
+            process.exit(1);
         }
         if (jhipsterVar.enableTranslation !== true) {
-          this.log(chalk.red('Error!') + ' The JHipster Fortune module only works when translation (i18n) is enabled');
-          process.exit(1);
+            this.log(chalk.red('Error! The JHipster Fortune module only works when translation (i18n) is enabled'));
+            process.exit(1);
         }
 
         const prompts = [
@@ -78,25 +78,25 @@ module.exports = generator.extend({
 
         this.userFortune = this.props.userFortune;
 
-        this.template('src/main/java/package/domain/_Fortune.java', javaDir + 'domain/Fortune.java');
-        this.template('src/main/java/package/repository/_FortuneRepository.java', javaDir + 'repository/FortuneRepository.java');
-        this.template('src/main/java/package/web/rest/_FortuneResource.java', javaDir + 'web/rest/FortuneResource.java');
-        this.template('src/main/resources/config/liquibase/_fortunes.csv', resourceDir + 'config/liquibase/fortunes.csv');
+        this.template('src/main/java/package/domain/_Fortune.java', `${javaDir}domain/Fortune.java`);
+        this.template('src/main/java/package/repository/_FortuneRepository.java', `${javaDir}repository/FortuneRepository.java`);
+        this.template('src/main/java/package/web/rest/_FortuneResource.java', `${javaDir}web/rest/FortuneResource.java`);
+        this.template('src/main/resources/config/liquibase/_fortunes.csv', `${resourceDir}config/liquibase/fortunes.csv`);
 
         this.changelogDate = jhipsterFunc.dateFormatForLiquibase();
-        this.template('src/main/resources/config/liquibase/changelog/_added_entity_Fortune.xml', resourceDir + 'config/liquibase/changelog/' + this.changelogDate + '_added_entity_Fortune.xml');
-        jhipsterFunc.addChangelogToLiquibase(this.changelogDate + '_added_entity_Fortune');
+        this.template('src/main/resources/config/liquibase/changelog/_added_entity_Fortune.xml', `${resourceDir}config/liquibase/changelog/${this.changelogDate}_added_entity_Fortune.xml`);
+        jhipsterFunc.addChangelogToLiquibase(`${this.changelogDate}_added_entity_Fortune`);
 
-        this.template('src/main/webapp/scripts/app/fortune/_fortune.controller.js', webappDir + 'app/fortune/fortune.controller.js');
-        this.template('src/main/webapp/scripts/app/fortune/_fortune.html', webappDir + 'app/fortune/fortune.html');
-        this.template('src/main/webapp/scripts/app/fortune/_fortune.js', webappDir + 'app/fortune/fortune.js');
-        this.template('src/main/webapp/scripts/app/fortune/_fortune.service.js', webappDir + 'app/fortune//fortune.service.js');
+        this.template('src/main/webapp/scripts/app/fortune/_fortune.controller.js', `${webappDir}app/fortune/fortune.controller.js`);
+        this.template('src/main/webapp/scripts/app/fortune/_fortune.html', `${webappDir}app/fortune/fortune.html`);
+        this.template('src/main/webapp/scripts/app/fortune/_fortune.js', `${webappDir}app/fortune/fortune.js`);
+        this.template('src/main/webapp/scripts/app/fortune/_fortune.service.js', `${webappDir}app/fortune//fortune.service.js`);
         jhipsterFunc.addElementToMenu('fortune', 'sunglasses', true, this.clientFramework);
         jhipsterFunc.addElementTranslationKey('fortune', 'Fortune', 'en');
         jhipsterFunc.addElementTranslationKey('fortune', 'Fortune', 'fr');
 
-        this.template('src/main/webapp/i18n/en/fortune.json', webappDir + 'i18n/en/fortune.json');
-        this.template('src/main/webapp/i18n/fr/fortune.json', webappDir + 'i18n/fr/fortune.json');
+        this.template('src/main/webapp/i18n/en/fortune.json', `${webappDir}i18n/en/fortune.json`);
+        this.template('src/main/webapp/i18n/fr/fortune.json', `${webappDir}i18n/fr/fortune.json`);
     },
 
     install() {
--- test/app.js
+++ test/app.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const fse = require('fs-extra');
-const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 const deps = [
